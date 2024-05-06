1b5193d160f2fd90e600c7b7cfe9de78be46177e
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-05-06 04:37:25 -0400

    ok

--- PatternGenerator.java
+++ PatternGenerator.java
@@ -55,7 +55,7 @@ public class PatternGenerator {
             } else {
                 generatePattern(n);
             }
-
+            
         System.out.println("\nDone.");
         scanner.close(); // Close the scanner
     }
